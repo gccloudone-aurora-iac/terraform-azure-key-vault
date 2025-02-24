62961cdd8c1c38c43a0cc901d62536a3d801fa9e
William H <sylus1984@gmail.com>
2025-02-24 10:10:28 -0500

    feat(ssc): Alignment with SSC standards (#1)

--- main.tf
+++ main.tf
@@ -46,9 +46,12 @@ resource "azurerm_private_endpoint" "this" {
   resource_group_name = var.resource_group_name
   subnet_id           = each.value.subnet_id
 
-  private_dns_zone_group {
-    name                 = "default"
-    private_dns_zone_ids = [each.value.private_dns_zone_id]
+  dynamic "private_dns_zone_group" {
+    for_each = each.value.private_dns_zone_id != null ? [1] : []
+      content {
+        name = "default"
+        private_dns_zone_ids = [each.value.private_dns_zone_id]
+    }
   }
 
   private_service_connection {
